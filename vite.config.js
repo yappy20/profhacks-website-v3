8ee0d5f0c1d8de2f57848a7cd299d8111ee833ae
Elisabeth Yap <ednamode2048@gmail.com>
2026-01-07 16:13:39 -0500

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/profhacks-website-v3/',
   server: {
     port: 8000,
     open: true
