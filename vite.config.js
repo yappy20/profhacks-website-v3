6c01311322153c1ec91f27dfce93f67f6092797f
Elisabeth Yap <ednamode2048@gmail.com>
2026-01-07 16:17:19 -0500

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/profhacks-website-v3/',
+  base: '/',
   server: {
     port: 8000,
     open: true
