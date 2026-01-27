a3c5377cc155b4832eebfd701ce9af93a8175590
Elisabeth Yap <ednamode2048@gmail.com>
2026-01-27 11:18:29 -0500

    Fix: Remove Home from footer, fix button bounds and gradient, update index.html for dev server

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,6 @@ function Footer() {
         <div className="footer-section">
           <h4>Quick Links</h4>
           <ul>
-            <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#schedule">Schedule</a></li>
             <li><a href="#prizes">Prizes</a></li>
